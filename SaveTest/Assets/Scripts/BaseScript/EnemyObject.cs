2ee40b8e1dabef6774e98fc3bcc24dadcb6e5deb
SpyroBlacky <SpyroBlacky@hotmail.com>
2014-02-13 00:43:47 +0900

    敵がエフェクトを吐きながら死ぬように
    自弾と敵とのあたり判定を修正
    敵爆発エフェクトの追加
    敵耐久ゼロでエフェクトとともに消滅するよう変更

--- SaveTest/Assets/Scripts/BaseScript/EnemyObject.cs
+++ SaveTest/Assets/Scripts/BaseScript/EnemyObject.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EnemyObject : MonoBehaviour {
 
 	public int HP;
+	public GameObject ParticleEnemyBreak;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,8 @@ public class EnemyObject : MonoBehaviour {
 	void Damage (int amount) {
 		HP -= amount;
 		if (HP <= 0) {
+			// 破壊エフェクト用オブジェクトをインスタンス化
+			Instantiate( ParticleEnemyBreak, transform.position, ParticleEnemyBreak.transform.rotation);
 			// オブジェクトを削除
 			Destroy(gameObject);
 		}
---
+++ SaveTest/Assets/Scripts/BaseScript/LifeTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeTime : MonoBehaviour {
+
+	public float Duration;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, Duration);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
